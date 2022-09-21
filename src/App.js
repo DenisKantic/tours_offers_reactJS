db1055bf9534e5543f7831bfc926a8131ea086e7
DenisKantic <denis.kantic18@gmail.com>
2022-09-21 19:40:44 +0200

    created loading.jsx file and added title and imported into App.js, it's working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
 import './App.css';
+import {useState, useEffect} from 'react';
+import Loading from './Loading';
+
+const url = 'https://course-api.com/react-tours-project';
 
 function App() {
+
+  const[tours, setTours] = useState([]);
+  const [loading,setLoading] = useState(false);
+
+  const fetchApi = async() =>{
+
+    setLoading(true);
+    const response = await fetch(url);
+    const tours = await response.json();
+    console.log(tours);
+    setLoading(false);
+    
+  }
+
+useEffect(()=>{
+  fetchApi();
+}, []);
+
+
+if(loading){
+ return ( 
+ <Loading></Loading>
+ );
+}
+
+
   return (
     <h1>Test</h1>
   );
---
+++ src/Loading.jsx
@@ -0,0 +1,9 @@
+import './App.css';
+
+const Loading = () => {
+    return ( 
+        <h1 className="loading">Loading</h1>
+     );
+}
+ 
+export default Loading;
